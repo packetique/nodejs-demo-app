e7769d8a4f3d57c1a47c8f6e3effc3e3a133eb20
Mikhail <packetique@gmail.com>
2021-03-02 17:24:56 +0300

    Added route '/info' into app.js

--- app/app.js
+++ app/app.js
@@ -12,8 +12,13 @@ router.get('/', function(req,res){
   res.render('index', {title: 'Nodejs Hello World', message: 'Hello World!!!', description: 'Node.js demo application', server_ip: 'Server ip: ' + ip.address()});
 });
 
+outer.get('/info', function(req,res){
+  res.render('info', {title: 'Information', os_type: 'OS type: ' + os.type(), release: "Release: " + os.release(),  server_ip: 'Server ip: ' + ip.address(), node_version: "Node.js version: " + process.version});
+});
+
 app.use(express.static(path));
 app.use('/', router);
+app.use('/info', router);
 app.listen(port, function () {
   console.log('Demo app listening on port ' + port)
 })
