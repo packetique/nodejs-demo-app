0c3b3e26aa4fbe8f3f220e5ffa3df2ab22523020
Mikhail <packetique@gmail.com>
2021-03-14 19:21:00 +0300

    Added console.log output for each router.get()

--- app/app.js
+++ app/app.js
@@ -11,14 +11,18 @@ app.set('view engine', 'pug');
 
 router.get('/', function(req,res){
   res.render('index', {title: 'Nodejs Hello World', message: 'Hello World!!!', description: 'Node.js demo application', server_ip: 'Server ip: ' + ip.address()});
+  console.log('get "/" ')
 });
+
 
 router.get('/info', function(req,res){
   res.render('info', {title: 'Information', os_type: 'OS type: ' + os.type(), release: "Release: " + os.release(),  server_ip: 'Server ip: ' + ip.address(), node_version: "Node.js version: " + process.version});
+  console.log('get "/info" ')
 });
 
 router.get('/about', function(req,res){
   res.render('about', {title: 'About this app'});
+  console.log('get "/about" ')
 });
 
 app.use(express.static(path));
